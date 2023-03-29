2a025369d256f04c0c3d57325384cc1667282a4a
KhokonPHb07 <kshehala.com>
2023-03-29 07:48:19 +0600

    fav icon and fontawesome added

--- src/componenents/Shop/Shop.jsx
+++ src/componenents/Shop/Shop.jsx
@@ -4,16 +4,19 @@ import './Shop.css'
 
 const Shop = () => {
     const[products,setProducts]=useState([]);
+    const[cart,setCart]=useState([]);
+
     useEffect( ()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     }, [])
-
+    // Marbo ekhanee las porbe soshanne..........aita kaj kortache differentvabe......very important 
     const handleAddToCart = (product) =>{
-        console.log(product);
+        const newCart=[...cart,product];
+        setCart(newCart);
     }
-
+    // .............................................................
 
     return (
         <div className='shop-container'>
@@ -28,6 +31,7 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                 <h4>Order summary</h4>
+                <p>Selected Item: {cart.length}</p>
             </div>
             
         </div>
